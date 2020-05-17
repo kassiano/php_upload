cf41c71d395bb4868ab9e68c8e30f5d5d7d39786
kassiano <kassiano.resende@gmail.com>
2020-05-17 17:58:16 -0300

    ok, erro msg

--- upload.php
+++ upload.php
@@ -4,8 +4,8 @@
      
     $file_path = $file_path . basename( $_FILES['FILE1']['name']);
     if(move_uploaded_file($_FILES['FILE1']['tmp_name'], $file_path)) {
-        echo "success";
+        echo "|OK|";
     } else{
-        echo "fail";
+        echo "|ERRO|";
     }
  ?>
